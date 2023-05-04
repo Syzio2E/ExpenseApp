92d69eca86aa88bf2fc857e84d3604ad934366e7
Your Name <shubhamsaurabhk@gmail.com>
2023-05-04 22:53:33 +0530

    first commit

---
+++ src/components/Layouts/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='signup_wrapper'>
+     <h1>Sign Up</h1>
+     <form>
+        <label htmlFor='username'>Username:</label>
+        <input type='text' id='username' name='username'/>
+        <label htmlFor='email'>Email:</label>
+        <input type='email' id='email' name='email'/>
+        <label htmlFor='password'>Password:</label>
+        <input type='password' id='password' name='password'/>
+        <button type='submit'>Sign Up</button>
+     </form>
+    </div>
+  )
+}
+
+export default Login
